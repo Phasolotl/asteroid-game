b44e9075290f762bb1d388ea0595fcb7fb3ee7ca
Phasolotl <enderaxgd@gmail.com>
2025-11-07 17:31:41 +0700

    Third Commit: Added groups (this took too long to understand)

--- asteroid_game/main.py
+++ asteroid_game/main.py
@@ -11,27 +11,34 @@ def main():
     dt = 0
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # group
+    updatable = pg.sprite.Group()
+    drawable = pg.sprite.Group()
+
     # player position
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
 
+    updatable.add(player)
+    drawable.add(player)
+
     while True:
         log_state()
+        dt = clock.tick(60) / 1000
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
-        Surface.fill(screen, (0,0,0))
-        ######################################
-        player.draw(screen)
-        player.update(dt)
-        ######################################
+        updatable.update(dt)
+
+        screen.fill((0,0,0))
+        for obj in drawable:
+            obj.draw(screen)
+
         display.flip()
         clock.tick(60)
-        dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
