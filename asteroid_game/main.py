cbb9127620fbc1b70b5db01d67db1f62f7de8b2e
Phasolotl <enderaxgd@gmail.com>
2025-11-16 16:25:09 +0700

    Ninth Commit: Modified the main file, hopefully nothing breaks

--- asteroid_game/main.py
+++ asteroid_game/main.py
@@ -12,70 +12,71 @@ from logger import log_state, log_event
 from player import Player, Shot
 
 
-def main():
-    pg.init()
-    clock = pg.time.Clock()
+class Game:
+    def __init__(self):
+        pg.init()
+        self.clock = pg.time.Clock()
 
-    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    # group
-    updatable = pg.sprite.Group()
-    drawable = pg.sprite.Group()
-    asteroids = pg.sprite.Group()
-    shots = pg.sprite.Group()
+        # group
+        self.updatable = pg.sprite.Group()
+        self.drawable = pg.sprite.Group()
+        self.asteroids = pg.sprite.Group()
+        self.shots = pg.sprite.Group()
 
-    Player.containers = (updatable, drawable)
-    Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = updatable
-    Shot.containers = (updatable, drawable, shots)
+        Player.containers = (self.updatable, self.drawable)
+        Asteroid.containers = (self.asteroids, self.updatable, self.drawable)
+        AsteroidField.containers = self.updatable
+        Shot.containers = (self.updatable, self.drawable, self.shots)
 
-    # Asteroid Field
+        # Asteroid Field
 
-    asteroid_field = AsteroidField()
+        self.asteroid_field = AsteroidField()
 
-    # player position
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+        # player position
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        self.player = Player(x, y)
 
-    dt = 0
+        self.dt = 0
 
-    while True:
-        log_state()
+    def run(self):
+        while True:
+            log_state()
 
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                return
+            for events in pg.event.get():
+                if events.type == pg.QUIT:
+                    return
 
-        updatable.update(dt)
+            self.updatable.update(self.dt)
 
-        for ast in asteroids:
-            if ast.collision(player):
-                log_event("player_hit")
-                print("Game over!")
-                t.sleep(1)
-                sys.exit()
+            for ast in self.asteroids:
+                if ast.collision(self.player):
+                    log_event("player_hit")
+                    print("Game over!")
+                    t.sleep(1)
+                    sys.exit()
 
-        for ast in asteroids:
-            for bullet in shots:
-                if bullet.collision(ast):
-                    log_event("asteroid_shot")
-                    bullet.kill()
-                    ast.split()
+            for ast in self.asteroids:
+                for bullet in self.shots:
+                    if bullet.collision(ast):
+                        log_event("asteroid_shot")
+                        bullet.kill()
+                        ast.split()
 
-        screen.fill((0,0,0))
+            self.screen.fill((0,0,0))
 
-        for bullet in shots:
-            bullet.draw(screen)
+            for bullet in self.shots:
+                bullet.draw(self.screen)
 
-        for obj in drawable:
-            obj.draw(screen)
+            for obj in self.drawable:
+                obj.draw(self.screen)
 
-        display.flip()
+            display.flip()
 
-        dt = clock.tick(60) / 1000
+            self.dt = self.clock.tick(60) / 1000
 
-if __name__ == "__main__":
-    main()
+Game().run()
 
 # end of code :3
