55a7253f5551555cefa2c520cbef81c16b52a507
Phasolotl <enderaxgd@gmail.com>
2025-11-23 19:16:37 +0700

    Third commit: It can run files now!

---
+++ AiAgent/functions/run_python_files.py
@@ -0,0 +1,31 @@
+import os, subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    try:
+        full_path = os.path.join(working_directory, file_path)
+        abs_working_directory = os.path.abspath(working_directory)
+        abs_file_path = os.path.abspath(full_path)
+
+        if not abs_file_path.startswith(abs_working_directory):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(abs_file_path):
+            return f'Error: File "{file_path}" not found.'
+        if not abs_file_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file.'
+
+        completed_process = subprocess.run(["python", abs_file_path, *args],
+                       cwd=abs_working_directory,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE,
+                       timeout=30,
+                       text=True)
+
+        if not completed_process.stdout and not completed_process.stderr:
+            return f"No output produced."
+        result = f"STDOUT: {completed_process.stdout}\nSTDERR: {completed_process.stderr}"
+        if completed_process.returncode != 0:
+            result += f"\nProcess exited with code {completed_process.returncode}"
+
+        return result
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- AiAgent/tests.py
+++ AiAgent/tests.py
@@ -1,17 +1,26 @@
 from functions.get_files_info import get_files_info
 from functions.get_files_content import get_file_content
 from functions.write_files import write_file
-
+from functions.run_python_files import run_python_file
 
 def main():
 
-    result1 = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    result2 = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    result3 = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    result1 = run_python_file("calculator", "main.py")
+    result2 = run_python_file("calculator", "main.py")
+    result3 = run_python_file("calculator", "main.py", ["3 + 5"])
+    result4 = run_python_file("calculator", "tests.py")
+    result5 = run_python_file("calculator", "../main.py")
+    result6 = run_python_file("calculator", "nonexistent.py")
+    result7 = run_python_file("calculator", "lorem.txt")
+
 
     print(result1)
     print(result2)
     print(result3)
+    print(result4)
+    print(result5)
+    print(result6)
+    print(result7)
 
 if __name__ == "__main__":
     main()
