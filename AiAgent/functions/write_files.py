7fb0ba980d45ec628ee273a15ee92f980a3aa412
Phasolotl <enderaxgd@gmail.com>
2025-11-23 18:33:45 +0700

    Second commit: It can write files now uh oh!

--- AiAgent/functions/write_files.py
+++ AiAgent/functions/write_files.py
@@ -1,2 +1,20 @@
+import os
+
 def write_file(working_directory, file_path, content):
-    pass
+    try:
+        full_path = os.path.join(working_directory, file_path)
+        abs_working_directory = os.path.abspath(working_directory)
+        abs_file_path = os.path.abspath(full_path)
+
+        if not abs_file_path.startswith(abs_working_directory):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(abs_file_path):
+            parent_direct = os.path.dirname(abs_file_path)
+            os.makedirs(parent_direct, exist_ok=True)
+
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as err:
+        return f"Error: {err}"
--- AiAgent/tests.py
+++ AiAgent/tests.py
@@ -1,18 +1,17 @@
 from functions.get_files_info import get_files_info
 from functions.get_files_content import get_file_content
+from functions.write_files import write_file
 
 
 def main():
 
-    result1 = get_file_content("calculator", "main.py")
-    result2 = get_file_content("calculator", "pkg/calculator.py")
-    result3 = get_file_content("calculator", "/bin/cat")
-    result4 = get_file_content("calculator", "pkg/does_not_exist.py")
+    result1 = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result2 = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    result3 = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
 
     print(result1)
     print(result2)
     print(result3)
-    print(result4)
 
 if __name__ == "__main__":
     main()
